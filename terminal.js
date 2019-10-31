dc19287a093472cb245e1f9c12492d05455dec74
joelpeckham <joelskyler@gmail.com>
2019-10-31 10:22:47 -0400

    getTermLine() function now works across wrapped lines.

--- terminal.js
+++ terminal.js
@@ -6,11 +6,12 @@ term.loadAddon(fitAddon);
 term.open(document.getElementById('terminal'));
 fitAddon.fit();
 
-
-function getTermLine(){
-	line = term.buffer.getLine(term.buffer.cursorY).translateToString(true)
-	return line
+function getTermLine(lineNum){
+	lineNum = (lineNum == undefined) ? term.buffer.cursorY : lineNum
+	line = term.buffer.getLine(lineNum)
+	lineString = ''
+	while (line.isWrapped){lineString = line.translateToString(true) + lineString; lineNum--; line = term.buffer.getLine(lineNum);}
+	return line.translateToString(true) + lineString;
 }
 
-
-term.onKey(e => {kbe = new KeyboardEvent('keypress', e.domEvent); document.getElementById("keyTrigger").dispatchEvent(kbe); console.log(kbe)});
+term.onKey(e => {kbe = new KeyboardEvent('keypress', e.domEvent); document.getElementById("keyTrigger").dispatchEvent(kbe);});
