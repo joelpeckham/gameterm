2bdbd6ee2dd3309eb337f183dbc214d2e916debb
joelpeckham <joelskyler@gmail.com>
2019-11-08 14:57:35 -0500

    Ripped out Brython's Spine!

--- terminal.js
+++ terminal.js
@@ -16,27 +16,4 @@ term.loadAddon(fitAddon);
 term.open(document.getElementById('terminal'));
 fitAddon.fit();
 
-function getTermLine(lineNum){
- 	lineNum = (lineNum == undefined) ? term.buffer.cursorY : lineNum
-	line = term.buffer.getLine(lineNum)
-	lineString = ''
-	while (line.isWrapped){lineString = line.translateToString(true) + lineString; lineNum--; line = term.buffer.getLine(lineNum);}
-	return line.translateToString(true) + lineString;
-}
-
-function getCursorY(){return term.buffer.cursorY;}
-
-function getTopTermLineNum(){return term.buffer.baseY;}
-
-function getLastTermLineNum(){
-	for (i = term.buffer.length - 1; i >= term.buffer.baseY; i--){
-		lineCandidate = term.buffer.getLine(i).translateToString().trim();
-		if(lineCandidate && lineCandidate != "") return i;
-	}
-	return 0;
-}
-
-term.onKey(e => {
-	let kbe = new KeyboardEvent('keypress', e.domEvent);
-	document.getElementById("keyTrigger").dispatchEvent(kbe);
-});
+term.write("Hello!")
