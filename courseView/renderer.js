d903057592bcd785eef1c99b0d1f8076a357aa59
joelpeckham <joelskyler@gmail.com>
2019-11-14 14:15:10 -0500

    Now spawns local terminal.

--- courseView/renderer.js
+++ courseView/renderer.js
@@ -4,3 +4,33 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+
+console.log("Neato in render!")
+
+var os = require('os');
+var pty = require('node-pty');
+var Terminal = require('xterm').Terminal;
+
+// Initialize node-pty with an appropriate shell
+const shell = process.env[os.platform() === 'win32' ? 'COMSPEC' : 'SHELL'];
+const ptyProcess = pty.spawn(shell, [], {
+  name: 'xterm-color',
+  cols: 80,
+  rows: 30,
+  cwd: process.cwd(),
+  env: process.env
+});
+
+// Initialize xterm.js and attach it to the DOM
+const xterm = new Terminal({	cursorStyle:'bar',
+											    fontSize:16,fontFamily:"'IBM Plex Mono', monospace",
+													allowTransparency:true,
+													theme:{background: 'rgba(255, 255, 255, 0.0)'}
+												  });
+xterm.open(document.getElementById('terminal'));
+
+// Setup communication between xterm.js and node-pty
+xterm.onData(data => ptyProcess.write(data));
+ptyProcess.on('data', function (data) {
+  xterm.write(data);
+});
--- courseView/terminal.js
+++ courseView/terminal.js
@@ -1,20 +1,20 @@
-class TerminalLine {
-  constructor(prompt, content) {
-    this.prompt = height;
-    this.content = content;
-  }
-}
-
-var term = new Terminal({	cursorStyle:'bar',
-													fontSize:16,fontFamily:"'IBM Plex Mono', monospace",
-													allowTransparency:true,
-													theme:{background: 'rgba(255, 255, 255, 0.0)'}
-												});
-
-var fitAddon = new FitAddon.FitAddon();
-
-term.loadAddon(fitAddon);
-term.open(document.getElementById('terminal'));
-fitAddon.fit();
-
-term.write("Hello!")
+// class TerminalLine {
+//   constructor(prompt, content) {
+//     this.prompt = height;
+//     this.content = content;
+//   }
+// }
+//
+// var term = new Terminal({	cursorStyle:'bar',
+// 													fontSize:16,fontFamily:"'IBM Plex Mono', monospace",
+// 													allowTransparency:true,
+// 													theme:{background: 'rgba(255, 255, 255, 0.0)'}
+// 												});
+//
+// var fitAddon = new FitAddon.FitAddon();
+//
+// term.loadAddon(fitAddon);
+// term.open(document.getElementById('terminal'));
+// fitAddon.fit();
+//
+// term.write("Hello!")
